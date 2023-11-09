7e31cd56df40ddf2b0b1bb301a33157dcdce8fe9
MICHAELCARRILLO <u20201a924@upc.edu.pe>
2023-11-09 09:30:50 -0500

    feat: init

---
+++ CreditoVehicularAPI/src/test/java/com/creditovehicular/creditovehicularapi/CreditoVehicularApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creditovehicular.creditovehicularapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditoVehicularApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
